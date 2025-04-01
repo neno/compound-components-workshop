2a5e117f4f1fe1b538d130862286efa4be7d2e54
Nenad Brcic <neno@neno.ch>
2025-04-01 16:26:11 +0200

    Extract helpers

--- src/components/pagination.tsx
+++ src/components/pagination.tsx
@@ -1,66 +1,32 @@
 import useQueryParam from "@/hooks/use-search-params";
-
-function getSearchParams() {
-  return new URLSearchParams(window.location.search);
-}
+import {
+  getNextUrl,
+  getPageNumbers,
+  getPrevUrl,
+  getUrlForPage,
+  isCurrentPage,
+  resetPagination,
+} from "@/utils/helpers";
+import { useEffect } from "react";
 
 const DEFAULT_LIMIT = 10;
-
-function hasPrevPage(offset: number) {
-  return offset > 0 && !isNaN(offset);
-}
-
-function hasNextPage(offset: number, limit: number, totalItems: number) {
-  return offset < totalItems - limit;
-}
-
-function getPrevUrl(offset: number, limit: number) {
-  if (!hasPrevPage(offset)) return undefined;
-
-  const searchParams = getSearchParams();
-  searchParams.set("offset", (offset - limit).toString());
-  searchParams.set("limit", limit.toString());
-  return `?${searchParams.toString()}`;
-}
-
-function getNextUrl(offset: number, limit: number, totalItems: number) {
-  if (!hasNextPage(offset, limit, totalItems)) return undefined;
-
-  const searchParams = getSearchParams();
-  searchParams.set("offset", (offset + limit).toString());
-  searchParams.set("limit", limit.toString());
-  return `?${searchParams.toString()}`;
-}
-
-function getPageCount(totalItems: number, limit: number) {
-  return Math.ceil(totalItems / limit);
-}
-
-function getPageNumbers(totalItems: number, limit: number) {
-  const pageCount = getPageCount(totalItems, limit);
-  return Array.from({ length: pageCount }, (_, i) => i);
-}
-
-function getUrlForPage(page: number, limit: number) {
-  const searchParams = getSearchParams();
-  searchParams.set("offset", (page * limit).toString());
-  searchParams.set("limit", limit.toString());
-  return `?${searchParams.toString()}`;
-}
-
-function isCurrentPage(page: number, offset: number, limit: number) {
-  return page === Math.floor(offset / limit);
-}
 
 export function Pagination({ totalItems }: { totalItems: number }) {
   const [offset] = useQueryParam("offset");
   const [limit] = useQueryParam("limit");
+  const [filter] = useQueryParam("filter");
 
   const limitNumber = Number(limit ?? DEFAULT_LIMIT);
   const offsetNumber = Number(offset ?? 0);
 
   const prevUrl = getPrevUrl(offsetNumber, limitNumber);
   const nextUrl = getNextUrl(offsetNumber, limitNumber, totalItems);
+
+  useEffect(() => {
+    if (filter) {
+      resetPagination(limitNumber);
+    }
+  }, [filter, limitNumber]);
 
   return (
     <div className="flex items-center gap-2">
---
+++ src/utils/helpers.ts
@@ -0,0 +1,56 @@
+export function getSearchParams() {
+  return new URLSearchParams(window.location.search);
+}
+
+export function hasPrevPage(offset: number) {
+  return offset > 0 && !isNaN(offset);
+}
+
+export function hasNextPage(offset: number, limit: number, totalItems: number) {
+  return offset < totalItems - limit;
+}
+
+export function getPrevUrl(offset: number, limit: number) {
+  if (!hasPrevPage(offset)) return undefined;
+
+  const searchParams = getSearchParams();
+  searchParams.set("offset", (offset - limit).toString());
+  searchParams.set("limit", limit.toString());
+  return `?${searchParams.toString()}`;
+}
+
+export function getNextUrl(offset: number, limit: number, totalItems: number) {
+  if (!hasNextPage(offset, limit, totalItems)) return undefined;
+
+  const searchParams = getSearchParams();
+  searchParams.set("offset", (offset + limit).toString());
+  searchParams.set("limit", limit.toString());
+  return `?${searchParams.toString()}`;
+}
+
+export function getPageCount(totalItems: number, limit: number) {
+  return Math.ceil(totalItems / limit);
+}
+
+export function getPageNumbers(totalItems: number, limit: number) {
+  const pageCount = getPageCount(totalItems, limit);
+  return Array.from({ length: pageCount }, (_, i) => i);
+}
+
+export function getUrlForPage(page: number, limit: number) {
+  const searchParams = getSearchParams();
+  searchParams.set("offset", (page * limit).toString());
+  searchParams.set("limit", limit.toString());
+  return `?${searchParams.toString()}`;
+}
+
+export function isCurrentPage(page: number, offset: number, limit: number) {
+  return page === Math.floor(offset / limit);
+}
+
+export function resetPagination(limit: number) {
+  const searchParams = getSearchParams();
+  searchParams.set("offset", "0");
+  searchParams.set("limit", limit.toString());
+  return `?${searchParams.toString()}`;
+}
